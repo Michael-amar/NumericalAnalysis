f8a6234de33d010723514e16d47e4fb2ba765295
Michael-amar <amarmic@post.bgu.ac.il>
2021-02-10 21:08:20 +0200

    assignment2 working!

---
+++ assignment1.py
@@ -0,0 +1,182 @@
+"""
+In this assignment you should interpolate the given function.
+"""
+import torch
+import numpy as np
+import time
+import random
+
+def euclidean_distance(x1:float,y1:float,x2:float,y2:float):
+    # return ((P1[0]-P2[0])**2 + (P1[1]-P2[1])**2) ** 0.5
+    print (y1)
+    print (y2)
+    x = ((x1-x2)**2)
+    y = ((y1-y2)**2)
+    z = x + y
+    return z**0.5
+    # return (((x1-x2)**2) + ((y1-y2)**2)) ** 0.5
+
+def inverse(m:torch.tensor):
+    m.inverse()
+
+#gets initial guesses for bisection and regula falsi
+#e.g finds a,b such that f(a)*f(b) < 0
+#the guesses are within range [0,1] , i can assume that because when solving the t cubic t is between 0,1
+def get_initial_guesses(f: callable):
+    neg = 0 
+    pos = 0
+    current = 0
+    step = 0.1
+    while True:
+        if f(current) > 0:
+            pos = current
+        elif f(current) < 0:
+            neg = current
+        if f(pos) * f(neg) < 0:
+            break
+        current+= step
+        if current >= 1.0:
+            current = 0 
+            step = step/10
+    return pos,neg
+
+def bisection(f,a,b,err):
+    while True:
+        z = 0.5 * (a + b)
+        if  f(a) * f(z) < 0:
+            b = z
+        else :
+            a = z
+        if  abs(b-a) < 2 * err:
+            break
+    return 0.5 * (a + b)
+
+def plot(ContolPoints, M3 , T):
+    pts = T.mm(M3).mm(ContolPoints).T
+    plt.plot(pts[0],pts[1])
+    plt.legend()
+    plt.show()
+
+class Assignment1:
+    def __init__(self):
+        """
+        Here goes any one time calculation that need to be made before 
+        starting to interpolate arbitrary functions.
+        """
+
+        pass
+
+    def interpolate(self, f: callable, a: float, b: float, n: int) -> callable:
+        def search_spline(x,splinesRange,nsplines):
+            for i in range(0,nsplines):
+                if splinesRange[i][0] <= x and splinesRange[i][1] >= x:
+                    return i
+            return -1
+
+        nsplines = (n-2)//2
+        P = (list(map(lambda x : [x,f(x)] , np.linspace(a,b,(nsplines*2+2)))))
+        C = torch.Tensor(P)
+        splinesRange = np.zeros(shape = (nsplines,2))
+        for i in range(0,nsplines): 
+            Ci = torch.stack([
+                C[i*2+1],
+                2*C[i*2+1] - C[i*2],
+                C[i*2+2],
+                C[i*2+3],])
+            splinesRange[i] = [C[i*2+1][0],C[i*2+3][0]]
+
+
+
+
+
+        #Bezier curve is a function from [0,1] to (x,y)
+        #we need to return functoin from x to y
+        #so given x we need to calculate its t and then find its y
+        # X(t) = (1-t)^3 * X0 + 3*(1-t)^2 * t * X1 + 3*(1-t) * t^2 * X2 + t^3 * X3
+        # all points are given so we can find t using bisection
+        #then plug t in the y function and get y coordinate
+        # Y(t) = (1-t)^3 * Y0 + 3*(1-t)^2 * t * Y1 + 3*(1-t) * t^2 * Y2 + t^3 * Y3
+
+        
+        
+        
+        def find_y(x):
+            spline_index = search_spline(x,splinesRange,nsplines)
+            if spline_index == -1:
+                return "x value out of curve"
+            x0,y0 = C[spline_index*2+1]
+            x1,y1 = 2*C[spline_index*2+1] - C[spline_index*2]
+            x2,y2 = C[spline_index*2+2]
+            x3,y3 = C[spline_index*2+3]
+            find_t = lambda x_ : (lambda t : ((1-t)**3 * x0) + (3*(1-t)**2 * t * x1) + (3*(1-t) * (t**2) * x2) + ((t**3) * x3) - x_ )
+            calc_y = lambda t: (((1-t)**3) * y0) + (3*((1-t)**2) * t * y1) + (3*(1-t) * (t**2) * y2) + ((t**3) * y3)
+            t_cubic = find_t(x)
+            pos,neg = get_initial_guesses(t_cubic)
+            desired_t = bisection(t_cubic,pos,neg,0.0000000001)
+            return float(calc_y(desired_t))
+
+        return find_y
+
+
+##########################################################################
+
+
+import unittest
+from functionUtils import *
+from tqdm import tqdm
+
+
+class TestAssignment1(unittest.TestCase):
+
+    def test_with_poly(self):
+        T = time.time()
+
+        ass1 = Assignment1()
+        mean_err = 0
+
+        d = 300
+        for i in tqdm(range(100)):
+            a = np.random.randn(d)
+
+            f = np.poly1d(a)
+
+            ff = ass1.interpolate(f, -10, 10, 300 + 1)
+
+            xs = np.random.random(200)
+            err = 0
+            for x in xs:
+                yy = ff(x)
+                y = f(x)
+                err += abs(y - yy)
+
+            err = err / 200
+            mean_err += err
+        mean_err = mean_err / 100
+
+        T = time.time() - T
+        print(T)
+        print(mean_err)
+
+    def test_with_poly_restrict(self):
+        ass1 = Assignment1()
+        a = np.random.randn(5)
+        f = RESTRICT_INVOCATIONS(10)(np.poly1d(a))
+        ff = ass1.interpolate(f, -10, 10, 10)
+        xs = np.random.random(20)
+        for x in xs:
+            yy = ff(x)
+
+
+if __name__ == "__main__":
+    ass1 = Assignment1()
+    f = lambda x : (x**2)-(3*x)+5 
+    
+    interpolated = ass1.interpolate(f,1,100,100)
+    for i in range (2,100):
+        original = float(f(i))
+        interpolateddd = float(interpolated(i))
+        err = 1000000
+        if type(interpolateddd) != str :   
+            err = (abs(original - interpolateddd))/original
+        print("original " + str(i) + ":" , original , "interpolated :" , interpolateddd , " relative error = " , err)
+    # unittest.main()
---
+++ assignment2.py
@@ -0,0 +1,105 @@
+"""
+In this assignment you should find the intersection points for two functions.
+"""
+import numpy as np
+import time
+import random
+from collections.abc import Iterable
+
+def derivative(f,x):    
+    h = 1e-8
+    return (f(x+h)-f(x))/h
+
+def includes_point(roots,root,epsilon):
+    for i in roots:
+        if abs(root-i) < epsilon:
+            return True
+    return False
+
+def NewtonRaphson(f,Xprev,err,timeout):
+    initial_time = time.time()
+    while (time.time() - initial_time) < timeout :
+        der = derivative(f,Xprev)
+        if abs(der) > 0.000001 :
+            Xnext = Xprev - ((f(Xprev))/(der))
+        else :
+            return "divide by zero error"
+        if abs(f(Xnext)) < err:
+            return Xnext
+        Xprev = Xnext
+    return "timeout"
+
+
+class Assignment2:
+    def __init__(self):
+        """
+        Here goes any one time calculation that need to be made before 
+        solving the assignment for specific functions. 
+        """
+
+        pass
+
+    def intersections(self, f1: callable, f2: callable, a: float, b: float, maxerr=0.001) -> Iterable:
+        roots = []
+        if a <= b:
+            min = a
+            max = b
+        else:
+            min = b
+            max = a
+        f = lambda x : (f1(x)-f2(x))
+        n = 10 + (((max-min)//10)*5 )              #longer range ==> more points
+        initial_guess = np.linspace(min,max,n)
+        for i in range (0,n):
+            try:
+                root = NewtonRaphson(f,initial_guess[i],maxerr,timeout=1.5)
+                if type(root) != str :
+                    if min <= root <= max:
+                        if not includes_point(roots,root,0.005):
+                            roots.append(root)
+            except Exception:
+                pass
+        return roots
+##########################################################################
+
+
+import unittest
+from sampleFunctions import *
+from tqdm import tqdm
+
+
+class TestAssignment2(unittest.TestCase):
+
+    def test_sqr(self):
+
+        ass2 = Assignment2()
+
+        f1 = np.poly1d([-1, 0, 1])
+        f2 = np.poly1d([1, 0, -1])
+
+        X = ass2.intersections(f1, f2, -1, 1, maxerr=0.001)
+        for x in X:
+            self.assertGreaterEqual(0.001, abs(f1(x) - f2(x)))
+
+    def test_poly(self):
+
+        ass2 = Assignment2()
+
+        f1, f2 = randomIntersectingPolynomials(10)
+
+        X = ass2.intersections(f1, f2, -1, 1, maxerr=0.001)
+
+        for x in X:
+            self.assertGreaterEqual(0.001, abs(f1(x) - f2(x)))
+
+if __name__ == "__main__":
+    # f = lambda x : (x**3) + (2*(x**2)) -0.5
+    # print(NewtonRaphson(f,0.5,0.001,timeout=10))
+
+    # f1 = lambda x: (x**3) + (2*(x**2)) -0.5
+    # f2 = lambda x: 0
+    # ass2=Assignment2()
+    # print(ass2.intersections(f1,f2,-5,10,0.001))
+
+     unittest.main()
+
